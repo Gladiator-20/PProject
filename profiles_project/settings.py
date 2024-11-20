2604a4a6793f26780df6e4d3364837b9a86d3675
Gladiator-20 <parohaaashutosh@gmail.com>
2024-11-20 17:22:48 +0530

    created Django project and app

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3=6i8&7sgb!jek9r$9jx_pmli47_txu*xc*p=ay9b-kw8@%6$$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 
