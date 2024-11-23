1ed13c977bff9ac6ffce345f6b6b7e63adf8f8d5
Gladiator-20 <parohaaashutosh@gmail.com>
2024-11-23 06:16:20 +0530

    updated setup file

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,7 +5,7 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/Gladiator-20/PProject.git'
 
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/PProject'
 
 # Set Ubuntu Language
 locale-gen en_GB.UTF-8
